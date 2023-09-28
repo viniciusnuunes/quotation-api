d315fd826f7f388b341f2e2cf97ad81d0fee487f
Vinicius <viniiciuspereiira@hotmail.com>
2023-09-27 23:53:58 -0300

    wip: returning a json message to request

--- handler/currency.go
+++ handler/currency.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,4 +12,5 @@ type CurrencyHandler struct {
 
 func (ch *CurrencyHandler) GetAllCurrencies(ctx *gin.Context) {
 	fmt.Println("cheguei no Handler")
+	ctx.JSON(http.StatusOK, gin.H{"message": "tudo deu certo"})
 }
